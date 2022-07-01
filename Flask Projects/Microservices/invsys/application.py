cb85da3aac79838cb4cc151b23d89cc401cc9503
Chris Harper <chrisharper0411@gmail.com>
2022-07-01 11:03:15 +0100

    hourly push.

--- Flask Projects/Microservices/invsys/application.py
+++ Flask Projects/Microservices/invsys/application.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from blueprints.continuous_resource_blueprint import create_continuous_resource_blueprint
+from database import db
 
-def create_app() -> Flask:
+def create_app(db_uri: str) -> Flask:
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    # We still need to initialise the db with the flask app, but we can do this after the object has been initialised by using init_app
+    db.init_app(app)
 
-    # Register continuous resource blueprints
+    with app.app_context():
+        db.create_all()
+
+    # Register cars continuous resource blueprints
     app.register_blueprint(
         create_continuous_resource_blueprint(
             blueprint_name="CarsBlueprint", # The name, used by flask when using the url_for function
@@ -14,7 +21,25 @@ def create_app() -> Flask:
         url_prefix='/api'
     )
 
-    # Then do the same for lorry and truck
-    ...
+    # Register lorrys continuous resource blueprints
+    app.register_blueprint(
+        create_continuous_resource_blueprint(
+            blueprint_name="LorrysBlueprint", # The name, used by flask when using the url_for function
+            resource_type="Lorry", # The resource type
+            resource_prefix="lorrys" # The base of the url for this resource type
+        ),
+        url_prefix='/api'
+    )
+
+
+    #Register trucks continuous resource blueprints
+    app.register_blueprint(
+        create_continuous_resource_blueprint(
+            blueprint_name="TrucksBlueprint", # The name, used by flask when using the url_for function
+            resource_type="Truck", # The resource type
+            resource_prefix="trucks" # The base of the url for this resource type
+        ),
+        url_prefix='/api'
+    )
 
     return app
